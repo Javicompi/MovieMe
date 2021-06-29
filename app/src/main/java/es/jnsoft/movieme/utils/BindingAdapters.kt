ac6484861aa7fce2e2b47b2989d32017d75dac28
Javicompi <javicompi@gmail.com>
2021-06-29 18:57:55 +0200

    Replaced Genre and added creators databinding function

--- app/src/main/java/es/jnsoft/movieme/utils/BindingAdapters.kt
+++ app/src/main/java/es/jnsoft/movieme/utils/BindingAdapters.kt
@@ -9,10 +9,11 @@ import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import es.jnsoft.movieme.R
-import es.jnsoft.movieme.data.network.model.movie.Movie.Genre
+import es.jnsoft.movieme.data.network.model.common.Genre
 import es.jnsoft.movieme.data.network.model.search.Search
 import es.jnsoft.movieme.data.network.model.trend.Trend
 import es.jnsoft.movieme.data.network.model.trend.TrendMediaType
+import es.jnsoft.movieme.data.network.model.tv.Tv.CreatedBy
 
 object BindingAdapters {
 
@@ -145,6 +146,18 @@ object BindingAdapters {
             }
             val result = listOfValues.joinToString(", ")
             result.replaceFirstChar { it.uppercase() }
+        }
+    }
+
+    @BindingAdapter("android:creators")
+    @JvmStatic
+    fun TextView.setCreators(creators: List<CreatedBy>?) {
+        text = if (creators.isNullOrEmpty()) {
+            ""
+        } else {
+            creators.joinToString(", ") {
+                it.name
+            }
         }
     }
 }
