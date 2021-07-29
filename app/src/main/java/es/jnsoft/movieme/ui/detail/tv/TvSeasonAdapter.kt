29c3b5d213edf965d7a00fb14095f66b7f2cefe9
Javicompi <javicompi@gmail.com>
2021-07-29 10:25:54 +0200

    Season Recyclerview expandable behaviour

--- app/src/main/java/es/jnsoft/movieme/ui/detail/tv/TvSeasonAdapter.kt
+++ app/src/main/java/es/jnsoft/movieme/ui/detail/tv/TvSeasonAdapter.kt
@@ -1,6 +1,7 @@
 package es.jnsoft.movieme.ui.detail.tv
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -8,14 +9,25 @@ import androidx.recyclerview.widget.RecyclerView
 import es.jnsoft.movieme.data.network.model.tv.Tv
 import es.jnsoft.movieme.databinding.ListItemSeassonBinding
 
-class TvSeasonAdapter() :
+class TvSeasonAdapter :
     ListAdapter<Tv.Season, TvSeasonAdapter.ViewHolder>(TvSeasonDiffUtilCallBack()) {
+
+    private var expandedPosition = RecyclerView.NO_POSITION
+    private var prevExpandedPosition = RecyclerView.NO_POSITION
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return ViewHolder.from(parent)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val isExpanded = position == expandedPosition
+        holder.binding.listItemSeassonDetails.visibility = if (isExpanded) View.VISIBLE else View.GONE
+        if (isExpanded) prevExpandedPosition = position
+        holder.itemView.setOnClickListener {
+            expandedPosition = if (isExpanded) RecyclerView.NO_POSITION else position
+            notifyItemChanged(prevExpandedPosition)
+            notifyItemChanged(expandedPosition)
+        }
         holder.bind(getItem(position))
     }
 
